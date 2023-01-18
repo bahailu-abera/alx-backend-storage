2a40bc8833177115d40ee0ce7772772d73381cfb
bahailu <aberabahailu4@gmail.com>
2023-01-18 19:56:25 +0300

    string match

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,3 @@
+-- script that lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name, split - formed as lifespan FROM metal_bands
+       WHERE LEFT(style, 9)='Glam rock' ORDER BY lifespan DESC;
