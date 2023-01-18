9e2899af33f9c83f91425a466dfda80926be5228
bahailu <aberabahailu4@gmail.com>
2023-01-18 13:46:53 +0300

    unique email

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,6 +2,6 @@
 -- id is unique and primary key
 CREATE TABLE IF NOT EXISTS users
        (id INT PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT,
-       email VARCHAR(255),
+       email VARCHAR(255) UNIQUE NOT NULL,
        name VARCHAR(255)
        );
