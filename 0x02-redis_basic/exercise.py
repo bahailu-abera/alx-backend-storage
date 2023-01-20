edea3d4d9cdfaa0517e42792b206f3b65d2e2f91
bahailu <aberabahailu4@gmail.com>
2023-01-20 11:33:44 +0300

    return annotation

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 Module Writting to redis
 """
 import redis
-import uuid
+from uuid import uuid4
 from typing import Union
 
 
@@ -15,11 +15,11 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]) -> uuid.UUID:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store the input data in Redis using random key and return the key
         """
-        key = uuid.uuid4()
-        self._redis.set(str(key), data)
+        randomkey = str(uuid4())
+        self._redis.set(randomkey, data)
 
-        return str(key)
+        return randomkey
