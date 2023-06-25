4899382c3b50d55ec796678428dc2ca6ccde149f
Bahailu Abera <aberabahailu4@gmail.com>
2023-06-25 21:41:51 +0300

    implement the store method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,66 +2,22 @@
 """
 Module Writting to redis
 """
-from functools import wraps
 import redis
+from typing import Union
 from uuid import uuid4
-from typing import Union, Callable, Optional
-
-
-def count_calls(method: Callable) -> Callable:
-    """
-    A decorator function
-    """
-    @wraps(method)
-    def wrapper(self, *args, **kwds):
-        key = method.__qualname__
-        self._redis.incr(key)
-        return method(self, *args, **kwds)
-    return wrapper
 
 
 class Cache:
-    """
-    Redis client object
-    """
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @count_calls
-    def store(self, data: Union[str, bytes, int, float]) -> str:
+    def store(self, data: Union[int, str, float, bytes]) -> str:
         """
-        Store the input data in Redis using random key and return the key
+        Stores the input data in a redis with a random key
+        and returns the key
         """
-        randomkey = str(uuid4())
-        self._redis.set(randomkey, data)
+        key = str(uuid4())
+        self._redis.set(key, data)
 
-        return randomkey
-
-    def get(self, key: str,
-            fn: Optional[Callable] = None) -> Union[bytes, float, int, str]:
-        """
-        Convert the data to the desired format
-        """
-        value = self._redis.get(key)
-        if fn is None or value is None:
-            return value
-        return fn(value)
-
-    def get_str(self, key: str) -> str:
-        """
-        Get a string from the cache
-        """
-        value = self._redis.get(key)
-        return value.decode('utf-8')
-
-    def get_int(self, key: str) -> int:
-        """
-        Get an int from the cache
-        """
-        value = self._redis.get(key)
-        try:
-            value = int(value.decode('utf-8'))
-        except Exception:
-            value = 0
-        return value
+        return key
