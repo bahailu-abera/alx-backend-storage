269b221e2f7e935d50794c034dd633fb87503ef1
Bahailu Abera <aberabahailu4@gmail.com>
2023-06-25 22:29:08 +0300

    feat: implement get, get_str and get_int methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 Module Writting to redis
 """
 import redis
-from typing import Union
+from typing import Union, Callable, Optional
 from uuid import uuid4
 
 
@@ -24,3 +24,36 @@ class Cache:
         self._redis.set(key, data)
 
         return key
+
+    def get(self, key: str, fn:
+            Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """
+        Used to convert back to the desired type
+        """
+        value = self._redis.get(key)
+
+        if fn is None or value is None:
+            return value
+
+        return fn(value)
+
+    def get_str(key: str) -> str:
+        """
+        Get a string from the Cache
+        """
+        value = self._redis.get(key)
+
+        return value.decode('utf-8')
+
+    def get_int(key: str) -> int:
+        """
+        Get an integer from the Cache
+        """
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode('utf-8'))
+
+        except ValueError, TypeError:
+            value = 0
+
+        return value
